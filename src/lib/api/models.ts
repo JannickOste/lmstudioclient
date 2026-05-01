69894a400a6ba118e7432695136f7204ce643cf4
Jannick Oste <oste.jannick@hotmail.com>
2026-05-01 18:13:34 +0200

    Download action added to ModelClient

--- src/lib/api/models.ts
+++ src/lib/api/models.ts
@@ -1,8 +1,25 @@
+import DownloadModelOptions from "../lmstudio/models/operations/download/DownloadModelOptions";
+import DownloadModelResult from "../lmstudio/models/operations/download/DownloadModelResult";
 import LMStudioClient from "./lmstudio";
 export default class ModelClient {
     public constructor(
         private readonly client: Readonly<LMStudioClient>
     ) {
 
+    }
+    
+    /** 
+     * Download LLMs and embedding models
+     * 
+     * endpoint: /api/v1/models/download 
+     * docs: https://lmstudio.ai/docs/developer/rest/download
+     * */
+    public download(
+        options: DownloadModelOptions
+    ): Promise<DownloadModelResult> {
+        return this.client.request("/api/v1/models/download", {
+            method: "POST",
+            body: JSON.stringify(options),
+        });
     }
 }
